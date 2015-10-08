f08bf07cd7a4bdcd9ea5473583b353995f171b3d
oisdk <oisin.kidney@gmail.com>
2015-10-09 00:17:17 +0100

    went mad with <$>

--- Monty Hall.hsproj/Monty_Hall.hs
+++ Monty Hall.hsproj/Monty_Hall.hs
@@ -4,11 +4,12 @@ import Data.List
 import Data.Ord
 import Data.Maybe
 import System.Random
+import Data.Tuple
 
 newtype Prob a = Prob { getProb :: [(a,Rational)] } deriving Show  
 
 instance Functor Prob where  
-  fmap f (Prob xs) = Prob [(f x,p)|(x,p) <- xs]
+  fmap f (Prob xs) = Prob $ fmapFst f <$> xs
     
 instance Applicative Prob where
   pure a = Prob [(a,1)]
@@ -21,20 +22,28 @@ instance Monad Prob where
 equalProbs :: [a] -> Prob a
 equalProbs x = Prob $ map (flip (,) (1%n))  x
   where n = fromIntegral (length x)
-        
+     
+fmapFst :: (a -> b) -> (a,c) -> (b,c)
+fmapFst f (a,b) = (f a, b)
+
+uncons :: [a] -> Maybe (a, [a])
+uncons []     = Nothing
+uncons (x:xs) = Just (x, xs)
+   
 mergeProbs :: Ord a => Prob a -> Prob a
-mergeProbs (Prob xs) = Prob $ mergeBy (comparing fst) addProb xs
-  where addProb (a,pa) (_,pb) = (a,pa+pb)
-          
+mergeProbs (Prob xs) = Prob $ mergeBy (comparing fst) (fmap . (+) . snd) xs
+
+isEq :: Ordering -> Bool
+isEq EQ = True
+isEq _  = False
+
 mergeBy :: (a -> a -> Ordering) -> (a -> a -> a) -> [a] -> [a]
-mergeBy _ _ [] = []
-mergeBy c m xs = mergeWith h [] t
-  where (h,t)  = (head sorted, tail sorted)
-        sorted = sortBy c xs
-        mergeWith h p [] = h:p
-        mergeWith h p (y:ys) = case c h y of
-          EQ        -> mergeWith (m h y) p ys
-          otherwise -> mergeWith y (h:p) ys
+mergeBy c m xs = mergeBySt eq m (sortBy c xs)
+  where eq x y = isEq $ c x y
+
+mergeBySt :: (a -> a -> Bool) -> (a -> a -> a) -> [a] -> [a]
+mergeBySt e c = unfoldr $ fmap sumUp . uncons
+  where sumUp (l,ls) = fmapFst (foldl' c l) (break (not . e l) ls)
 
 data Choice = Switch | Stick
 
@@ -56,3 +65,6 @@ choose (Prob x) = (fromN x) <$> fromIntegral <$> (getStdRandom (randomR (1,10000
               | v < 0     = x
               | otherwise = fromN xs v
               where v = n - p * 1000000
+              
+
+                                       
