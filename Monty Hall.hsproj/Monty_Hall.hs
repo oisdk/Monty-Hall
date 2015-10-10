5d3423e813512d1bfb78c72979b06ef319ffeaf6
oisdk <oisin.kidney@gmail.com>
2015-10-10 16:14:57 +0100

    wspace

--- Monty Hall.hsproj/Monty_Hall.hs
+++ Monty Hall.hsproj/Monty_Hall.hs
@@ -12,11 +12,13 @@ instance Functor Prob where
     
 instance Applicative Prob where
   pure a = Prob [(a,1)]
-  (Prob ys) <*> (Prob xs) = Prob [(f x, px*py)|(f,py) <- ys, (x,px) <- xs]
+  (Prob ys) <*> (Prob xs) = Prob [(f x, px*py)|(f,py) <- ys, 
+                                               (x,px) <- xs]
   
 instance Monad Prob where
   return = pure
-  (Prob xs) >>= f = Prob [(y,px*py)|(x,px) <- xs, (y,py) <- getProb(f x)]
+  (Prob xs) >>= f = Prob [(y,px*py)|(x,px) <- xs, 
+                                    (y,py) <- getProb(f x)]
   
 equalProbs :: [a] -> Prob a
 equalProbs x = Prob $ (flip (,) n) <$> x
@@ -33,7 +35,9 @@ eqOf c = (\a b -> case c a b of EQ -> True
                                 _  -> False)
 
 mergeBy :: (a -> a -> a) -> (a -> a -> Ordering) -> [a] -> [a]
-mergeBy m c = (foldl1' m <$>) . (groupBy $ eqOf c) . (sortBy c)
+mergeBy m c = (foldl1' m <$>)  . 
+              groupBy (eqOf c) . 
+              sortBy c
 
 data Choice = Switch | Stick
 
@@ -44,7 +48,7 @@ chances n p Switch d = (&&) . not          <$>
                        (equalProbs $ True : replicate (n-p-2) False)
 
 chanceOfCar :: Int -> Int -> Choice -> Prob Bool
-chanceOfCar n p s = mergeProbs $
+chanceOfCar n p s = mergeProbs          $
                     equalProbs [1..n] >>= 
                     chances n p s
 
@@ -52,6 +56,11 @@ randRatio :: Integer -> IO Rational
 randRatio d = (%d) <$> getStdRandom (randomR (1,d))
 
 choose :: Prob a -> IO a
-choose = (<$> randRatio 1000) . flip upTo . scanl1 sumSnd . getProb
-   where upTo n = fst . fromJust . find ((>n) . snd)
+choose = (<$> randRatio 1000) .
+         flip upTo            . 
+         scanl1 sumSnd        . 
+         getProb
+   where upTo n = fst         . 
+                  fromJust    . 
+                  find ((> n) . snd)
               
